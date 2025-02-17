cd3d4324c449476a646e20b8b88f67d940124075
Dani Yanani <alejandrorot2@gmail.com>
2025-02-17 12:16:32 +0100

    Lista_con_API

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,28 +1,51 @@
-import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, { useState } from "react";
 
 //create your first component
-const Home = () => {
-	return (
-		<div className="text-center">
-            
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+const Home = () => {
+  const [inputValue, setInputValue] = useState("");
+
+  const [tareas, setTareas] = useState([]);
+
+  return (
+    <div className="list" style={{ width: "500px" }}>
+      <h1>Mis tareas </h1>
+
+      <ul>
+        <li>
+          {" "}
+          <input
+            type="text"
+            onChange={(e) => setInputValue(e.target.value)}
+            value={inputValue}
+            onKeyDown={(e) => {
+              if (e.key === "Enter") {
+                setTareas(tareas.concat(inputValue));
+                setInputValue("");
+              }
+            }}
+            placeholder="añade una tarea"
+          ></input>
+        </li>
+
+        {tareas.map((t, index) => (
+          <li key={index}>
+            {t}{" "}
+            <img
+              src="https://raw.githubusercontent.com/4GeeksAcademy/Lista_De_Tareas/refs/heads/main/marca-x.png"
+              style={{ width: "15px", height: "15" }}
+              onClick={() =>
+                setTareas(
+                  tareas.filter((t, currentIndex) => index != currentIndex)
+                )
+              }
+            />
+          </li>
+        ))}
+      </ul>
+      <div>{tareas.length} tareas </div>
+    </div>
+  );
 };
 
 export default Home;
